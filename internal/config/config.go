59225069b767879c9e7791c5740c228fbdd14d7b
liaoyongkun <etc.liao@gmail.com>
2026-04-28 14:34:11 +0800

    feat: 读取配置

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,13 +1,32 @@
 package config
 
-import "github.com/spf13/viper"
+import (
+	"fmt"
+	"time"
+
+	"github.com/spf13/viper"
+)
 
 type Config struct {
-	Server ServerConfig
+	Server   ServerConfig
+	Database DatabaseConfig
 }
 
 type ServerConfig struct {
 	Port int
+}
+
+type DatabaseConfig struct {
+	Host         string
+	User         string
+	Password     string
+	Name         string
+	Port         int
+	SSLMode      string
+	TimeZone     string
+	MaxIdleConns int
+	MaxOpenConns int
+	MaxLifeTime  time.Duration `mapstructure:"max_life_time"`
 }
 
 var Conf Config
@@ -24,4 +43,21 @@ func Init() {
 	if err := viper.Unmarshal(&Conf); err != nil {
 		panic(err)
 	}
+}
+
+func (c DatabaseConfig) DSN() string {
+	return fmt.Sprintf(
+		"host=%s user=%s password=%s dbname=%s port=%d sslmode=%s TimeZone=%s",
+		c.Host,
+		c.User,
+		c.Password,
+		c.Name,
+		c.Port,
+		c.SSLMode,
+		c.TimeZone,
+	)
+}
+
+func (c ServerConfig) PORT() string {
+	return fmt.Sprintf(":%d", c.Port)
 }
